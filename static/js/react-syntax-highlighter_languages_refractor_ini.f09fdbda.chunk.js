7e46d92029723bba92b15276015cf5a954ac1caf
RayRuizheLi <li40qt@hotmail.com>
2022-12-03 19:24:47 -0500

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_ini.f09fdbda.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkray_li_cs=self.webpackChunkray_li_cs||[]).push([[6495],{3409:function(t){function n(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=n,n.displayName="ini",n.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.f09fdbda.chunk.js.map
